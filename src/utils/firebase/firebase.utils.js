ee63af6d625e0a915e2b4f423aeb53a3d45c13d2
Brandon Nelson <jimar@Brandons-MacBook-Pro.local>
2023-06-15 16:35:00 -0500

    created authentication page component with sign in form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
     signInWithRedirect, 
     signInWithPopup,
     GoogleAuthProvider, 
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -76,4 +77,10 @@ const firebaseConfig = {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  export const signInAuthWithEmailAndPassword = async ( email, password) => {
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
   };
